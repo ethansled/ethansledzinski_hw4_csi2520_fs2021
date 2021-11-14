4e51cb901a38fe6d6e5cc037ff5ed1a67d59ea36
Ethan Sledzinski <ethansled@gmail.com>
2021-11-14 15:20:36 -0500

    added dynamic comment section and changed overall css styling & fixed formatting

--- js/blog.js
+++ js/blog.js
@@ -1,71 +1,75 @@
-window.onload = function setTemplate() {
-    document.getElementById('allComments').innerHTML = localStorage.getItem('template');
-};
+window.onload = function(){
+    document.getElementById('usComments').innerHTML
+}
 
-const commentContainer = document.getElementById('allComments');
-document.getElementById('addComments').addEventListener('click', function (ev) {
+
+
+
+const usComments = document.getElementById('usComments');
+document.getElementById('pComments').addEventListener('click', function (ev) 
+{
    addComment(ev);
-});
+})
+
+var postButton = document.getElementById('pComments');
+var commentText = document.getElementById('commentZone');
+var commentButton = document.getElementById('lComment');
+
+commentButton.onclick = function Comment(){
+    commentText.style.display = "block";
+    postButton.style.display = "block";
+    postButton.onclick = function HideCommentBox(){
+        commentText.style.display = "none";
+        postButton.style.display = "none";
+    }
+}
 
 function addComment(ev) {
-    let commentText, wrapDiv;
+    let commentText;
+    let wrapDiv;
     const textBox = document.createElement('div');
-    const replyButton = document.createElement('button');
-    replyButton.className = 'reply';
-    replyButton.innerHTML = 'Reply';
-    const deleteButton = document.createElement('button');
-    deleteButton.innerHTML = 'Delete';
-    deleteButton.className = 'deleteComment';
-    if(hasClass(ev.target.parentElement, 'container')) {
+    const reply = document.createElement('button');
+    reply.className = 'reply';
+    reply.innerHTML = 'Reply';
+    reply.style.marginLeft = "50%";
+
+    if(createClass(ev.target.parentElement, 'conta')) {
         const wrapDiv = document.createElement('div');
         wrapDiv.className = 'wrapper';
-        wrapDiv.style.marginLeft = 0;
-        commentText = document.getElementById('newComment').value;
-        document.getElementById('newComment').value = '';
+        commentText = document.getElementById('commentZone').value;
+        document.getElementById('commentZone').value = '';
         textBox.innerHTML = commentText;
-        textBox.style.backgroundColor = "#ff895d";
-        wrapDiv.append(textBox, replyButton, deleteButton);
-        commentContainer.appendChild(wrapDiv);
+        wrapDiv.append(textBox, reply);
+        usComments.appendChild(wrapDiv);
+        textBox.style.background = "#ff895d";
+
     } else {
         wrapDiv = ev.target.parentElement;
         commentText = ev.target.parentElement.firstElementChild.value;
         textBox.innerHTML = commentText;
-        textBox.style.backgroundColor = "#ff895d";
         wrapDiv.innerHTML = '';
-        wrapDiv.append(textBox, replyButton, deleteButton);
-    }
-    setOnLocalStorage();
+        wrapDiv.append(textBox, reply);
+        textBox.style.background = "#ff895d";
+    }   
 }
 
-function setOnLocalStorage () {
-    localStorage.setItem('template', document.getElementById('allComments').innerHTML);
+function createClass(element, className) {
+    return element.className.split(' ').indexOf(className) > -1;
 }
-function hasClass(elem, className) {
-    return elem.className.split(' ').indexOf(className) > -1;
-}
-document.getElementById('allComments').addEventListener('click', function (e) {
-    if (hasClass(e.target, 'reply')) {
+document.getElementById('usComments').addEventListener('click', function (e) {
+    if (createClass(e.target, 'reply')) {
         const parentDiv = e.target.parentElement;
         const wrapDiv = document.createElement('div');
-        wrapDiv.style.marginLeft = (Number.parseInt(parentDiv.style.marginLeft) + 15).toString() + 'px';
         wrapDiv.className = 'wrapper';
         const textArea = document.createElement('textarea');
-        textArea.style.marginRight = '20px';
         const addButton = document.createElement('button');
         addButton.className = 'addReply';
-        addButton.innerHTML = 'Add';
-        const cancelButton = document.createElement('button');
-        cancelButton.innerHTML = 'Cancel';
-        cancelButton.className='cancelReply';
-        wrapDiv.append(textArea, addButton, cancelButton);
-        parentDiv.appendChild(wrapDiv);
-
-    } else if(hasClass(e.target, 'addReply')) {
+        addButton.innerHTML = 'Post Reply';       
+        wrapDiv.append(textArea, addButton);
+        parentDiv.appendChild(wrapDiv);     
+        textArea.style.backgroundColor = "#ff895d";        
+    } else if(createClass(e.target, 'addReply')) {
         addComment(e);
-    } else if(hasClass(e.target, 'cancelReply')) {
-        e.target.parentElement.innerHTML = '';
-        setOnLocalStorage();
-    } else if(hasClass(e.target, 'deleteComment')) {
-        e.target.parentElement.remove();
-    }
-});
+    } 
+})
+
